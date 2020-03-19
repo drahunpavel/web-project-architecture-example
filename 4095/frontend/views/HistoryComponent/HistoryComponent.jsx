13d91f4b6e3d675c6baf53e50d522500c151fa3b
drahunpavel <drahunpavel@gmail.com>
2020-03-19 12:22:15 +0300

    upd historyComponent

--- 4095/frontend/views/HistoryComponent/HistoryComponent.jsx
+++ 4095/frontend/views/HistoryComponent/HistoryComponent.jsx
@@ -4,61 +4,49 @@ import('./HistoryComponent.scss');
 
 class HistoryComponent extends PureComponent {
 
+    state = {
+        selectedRequest: null
+    };
+
+    onHandleClick = (event) => {
+
+        console.log('--select', event.target.dataset.id)
+        console.log('--delete', event.target.dataset.delete)
+    };
+
     render() {
         return (
                 <div className="sidebar">
                     <div className='HistoryComponent'>
                         <h3>Custom Postman</h3>
                         <button className="btn btn-primary" type="submit">History</button>
+                        <button className="btn btn-danger" type="submit">Clear All</button>
                         <div className='list-wrapper'>
-                            <span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-primary">GET</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-primary">GET</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-primary">GET</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-primary">GET</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-primary">GET</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-primary">GET</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-primary">GET</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-primary">GET</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
-                            <h6><span class="badge badge-warning">POST</span>http://localhost:3000/api/updateProfile<span class="badge badge-secondary">X</span></h6>
+                            <h6 onClick={this.onHandleClick} data-id='1'>
+                                <span className="badge badge-warning" data-id='1'>POST</span>
+                                <span data-id='1'>http://localhost:3000/api/updateProfile</span>
+                                <span data-delete='1' className="badge badge-secondary">X</span>
+                            </h6>
+                            <h6 onClick={this.onHandleClick} data-id='2'>
+                                <span data-id='2' className="badge badge-warning">POST</span>
+                                <span data-id='2'>http://localhost:3000/api/updateProfile</span>
+                                <span data-delete='2' className="badge badge-secondary">X</span>
+                            </h6>
+                            <h6 onClick={this.onHandleClick} data-id='3'>
+                                <span data-id='3' className="badge badge-warning">POST</span>
+                                <span data-id='3'>http://localhost:3000/api/updateProfile</span>
+                                <span data-delete='3' className="badge badge-secondary">X</span>
+                            </h6>
+                            <h6 onClick={this.onHandleClick} data-id='4'>
+                                <span data-id='4' className="badge badge-warning">POST</span>
+                                <span data-id='4'>http://localhost:3000/api/updateProfile</span>
+                                <span data-delete='4' className="badge badge-secondary">X</span>
+                            </h6>
+                            <h6 onClick={this.onHandleClick} data-id='5'>
+                                <span data-id='5' className="badge badge-primary">GET</span>
+                                <span data-id='5'>http://localhost:3000/api/updateProfile</span>
+                                <span data-delete='5' className="badge badge-secondary">X</span>
+                            </h6>
                         </div>
                     </div>
                 </div>
