2dc51e4ac6d917899b382ba0067558d603e64289
drahunpavel <drahunpavel@gmail.com>
2020-03-21 13:51:16 +0300

    init back

---
+++ 4095/backend/utils/utils.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+// пишет строку в файл лога и одновременно в консоль
+
+// синхронная версия
+function logLineSync(logFilePath,logLine) {
+    const logDT=new Date();
+    let time=logDT.toLocaleDateString()+" "+logDT.toLocaleTimeString();
+    let fullLogLine=time+" "+logLine;
+
+    console.log(fullLogLine); // выводим сообщение в консоль
+
+    const logFd = fs.openSync(logFilePath, 'a+'); // и это же сообщение добавляем в лог-файл
+    fs.writeSync(logFd, fullLogLine + os.EOL); // os.EOL - это символ конца строки, он разный для разных ОС
+    fs.closeSync(logFd);
+};
+
+module.exports = {
+    logLineSync
+};
