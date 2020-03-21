be4b72b6d1eda8eb469ab1b367c746d8a0f8583f
drahunpavel <drahunpavel@gmail.com>
2020-03-21 13:28:43 +0300

    fix

--- 4095/frontend/views/DataInputComponent/DataInputComponent.jsx
+++ 4095/frontend/views/DataInputComponent/DataInputComponent.jsx
@@ -47,8 +47,7 @@ class DataInputComponent extends PureComponent {
             requestURLParams, 
             requestHeadersParams,
             formDataParams, 
-            urlencodedParams, 
-            requestHeadersParams 
+            urlencodedParams
         } = this.state.params;
 
         
