13f971213a5468865c5315f665cd40f0b565d129
drahunpavel <drahunpavel@gmail.com>
2020-03-23 17:13:41 +0300

    upd

--- 4095/frontend/core/axios.js
+++ 4095/frontend/core/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 const host = window.location.href;
 
-axios.defaults.baseURL = 'http://localhost:4096/';
+// axios.defaults.baseURL = 'http://localhost:4096/';
+axios.defaults.baseURL = host;
 
 export default axios;
