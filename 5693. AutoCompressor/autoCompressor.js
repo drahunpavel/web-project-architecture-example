dfa005a711cc9e12be2dc562e5f05ecf27744079
drahunpavel <drahunpavel@gmail.com>
2020-05-12 02:46:38 +0300

    upd

--- 5693. AutoCompressor/autoCompressor.js
+++ 5693. AutoCompressor/autoCompressor.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const fsp = fs.promises;
 const readline = require('readline');
 const { createGzip } = require('zlib');
 const { pipeline } = require('stream');
@@ -15,91 +16,94 @@ const FgYellow = "\x1b[33m";
 const FgBlue = "\x1b[34m";
 const BgRed = "\x1b[41m";
 
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 rl.question('Entry path: ', async path => {
-
-    readDir(path);
+    await readDir(path);
 });
 
-// readDir(_path)
-//     .then((res) => {
-//         console.log('--readDir is running, path: ', path);
-//     })
-//     .catch(err => { console.error(err); });
 
-//-----------------------------------------------------
 async function readDir(_path) {
 
-    return new Promise((resolve, reject) => {
-        fs.readdir(_path, (error, result) => {
+    //список файлов в папке
+    let listDir = await readdirAsync(_path);
+
+    for (const fileName of listDir) {
+
+        let filePath = path.join(_path, fileName); //путь к каждому файлу
+        const stats = await fsp.stat(filePath); //данные по файлу
+
+        if (stats.isFile()) {
+
+            let originalFilebirthtime;
+            let compressedFilebirthtime;
+            let brokenFile = fileName.split('.');
+
+            //не сжатые файлы
+            if (brokenFile[brokenFile.length - 1] != 'gz') {
+
+                let сompressedFile = listDir.find(item => item === `${fileName}.gz`);//поиск сжатого файла
+                originalFilebirthtime = stats.birthtimeMs; //дата создания файла оригинала
+
+                if (сompressedFile) {
+
+                    //получает данные по сжатому файлу
+                    fs.stat(`${filePath}.gz`, сompressedFile, (err, stat) => {
+                        compressedFilebirthtime = stat.birthtimeMs; //дата создания сжатого файла
+
+                        //проверка даты создания файла оригинала и сжататого файла
+                        //если дата создания файл оригинала свежее сжатого файла
+                        //удаляется старый сжататый файл и созадется новый 
+                        if (compressedFilebirthtime >= originalFilebirthtime) {
+                            console.log(FgGreen, '--compressed file is fine: ', 'fileName: ', fileName, '     path: ', filePath);
+                        } else {
+                            fs.unlink(`${filePath}.gz`, (err) => {
+                                if (err) throw err;
+
+                                console.log(BgRed, '--old compressed file deleted: ', 'fileName: ', fileName, '     path: ', filePath);
+
+                                do_gzip(filePath, `${filePath}.gz`, fileName)
+                                    // .then((res) => {
+                                    //     console.log(BgCyan, '--compressed file created: ', 'fileName: ', fileName, '     path: ', `${filePath}.gz`);
+                                    // })
+                                    .catch((err) => {
+                                        console.error('An error occurred:', err);
+                                        process.exitCode = 1;
+                                    });
+                            });
+                        };
+                    });
+                } else {
+                    console.log(FgBlue, '--file doesn`t have a compressed version: ', 'fileName: ', fileName, '     path: ', filePath);
+
+                    await do_gzip(filePath, `${filePath}.gz`, fileName)
+                        // .then((res) => {
+                        //     console.log(BgCyan, '--compressed file created: ', 'fileName: ', fileName, '     path: ', `${filePath}.gz`);
+                        // })
+                        .catch((err) => {
+                            console.error('An error occurred:', err);
+                            process.exitCode = 1;
+                        });
+                };
+            };
+        };
+        if (stats.isDirectory()) {
+            await readDir(filePath);
+        };
+    };
+};
+
+//get list of files function
+function readdirAsync(path) {
+    return new Promise(function (resolve, reject) {
+        fs.readdir(path, function (error, result) {
             if (error) {
                 reject(error);
             } else {
-
-                result.forEach((fileName) => {
-                    let filePath = path.join(_path, fileName);
-
-                    fs.stat(filePath, fileName, (err, stats) => {
-
-                        let brokenFile = fileName.split('.');
-
-                        if (stats.isFile()) {
-
-                            let originalFilebirthtime;
-                            let compressedFilebirthtime;
-
-                            //не сжатые файлы
-                            if (brokenFile[brokenFile.length - 1] != 'gz') {
-
-                                let сompressedFile = result.find(item => item === `${fileName}.gz`);//нашли сжатый вариант этого файла
-                                originalFilebirthtime = stats.birthtimeMs; //дата создания файла оригинала
-
-                                if (сompressedFile) {
-
-                                    //получает данные по сжатому файлу
-                                    fs.stat(`${filePath}.gz`, сompressedFile, (err, stat) => {
-                                        compressedFilebirthtime = stat.birthtimeMs; //дата создания сжатого файла
-
-                                        //проверка даты создания файла оригинала и сжататого файла
-                                        //если дата создания файл оригинала свежее сжатого файла
-                                        //удаляется старый сжататый файл и созадется новый 
-                                        if (compressedFilebirthtime >= originalFilebirthtime) {
-                                            console.log(FgGreen, '--compressed file is fine: ', 'fileName: ', fileName, '     path: ', filePath)
-                                            
-                                        } else {
-                                            fs.unlink(`${filePath}.gz`, (err) => {
-                                                if (err) throw err;
-
-                                                console.log(BgRed, '--old compressed file deleted: ', 'fileName: ', fileName, '     path: ', filePath);
-
-                                                do_gzip(filePath, `${filePath}.gz`, fileName)
-                                                    .catch((err) => {
-                                                        console.error('An error occurred:', err);
-                                                        process.exitCode = 1;
-                                                    });
-                                            });
-                                        };
-                                    });
-                                } else {
-                                    console.log(FgBlue, '--file doesn`t have a compressed version: ', 'fileName: ', fileName, '     path: ', filePath);
-
-                                    do_gzip(filePath, `${filePath}.gz`, fileName)
-                                        .catch((err) => {
-                                            console.error('An error occurred:', err);
-                                            process.exitCode = 1;
-                                        });
-                                }
-                            };
-                        };
-                        if (stats.isDirectory()) {
-                            readDir(filePath);
-                        };
-                    });
-                });
                 resolve(result);
             }
         });
@@ -109,10 +113,12 @@ async function readDir(_path) {
 //Compression function
 async function do_gzip(input, output, fileName) {
 
-    console.log(FgYellow, '--create new compressed file: ', 'fileName: ', fileName, '     path: ', input);
+    console.log(FgYellow, '--create new compressed file: ', 'fileName: ', fileName, '     path: ', `${input}.gz`);
 
     const gzip = createGzip();
     const source = createReadStream(input);
     const destination = createWriteStream(output);
     await pipe(source, gzip, destination);
+
+    console.log(FgYellow, '--compressed file created: ', 'fileName: ', fileName, '     path: ', `${input}.gz`);
 };
