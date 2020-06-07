1826833fc2d044560eaf51388b852b1edcc049a3
drahunpavel <drahunpavel@gmail.com>
2020-06-07 21:43:35 +0300

    EntryFieldBlock

--- 6195. SQLExplorer/frontend/components/EntryFieldBlock/EntryFieldBlock.jsx
+++ 6195. SQLExplorer/frontend/components/EntryFieldBlock/EntryFieldBlock.jsx
@@ -1,5 +1,20 @@
 import React, { Fragment } from "react";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+import Button from "@material-ui/core/Button";
+
+import("../../index.scss");
 
 export const EntryFieldBlock = () => {
-  return <Fragment>EntryFieldBlock</Fragment>;
+  return (
+    <div className="entryFieldBlock-content">
+      <div className="controlPanel">
+        <Button variant="contained" color="primary" size="small">
+          Send
+        </Button>
+      </div>
+      <div className="entry">
+        <textarea />
+      </div>
+    </div>
+  );
 };
